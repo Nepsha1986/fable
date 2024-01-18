20e7d61586d85c37d5e64e048ad1b0bdaef97102
Alex Nepsha <nepsha1986@gmail.com>
2024-01-18 18:15:14 +0300

    Improve Dialog component

--- src/components/SiteHeader/components/AboutInfo/index.tsx
+++ src/components/SiteHeader/components/AboutInfo/index.tsx
@@ -14,26 +14,24 @@ const AboutInfo = () => {
     <>
       <Button onClick={handleClick}>About</Button>
 
-      {isActive && (
-        <Dialog
-          heading={'About'}
-          open={isActive}
-          onClickClose={() => {
-            setIsActive(false);
-          }}
-        >
-          <p>
-            This project showcases a unique approach to implementing the
-            parallax effect through the manipulation of the "perspective-origin"
-            CSS property within the encompassing container. The essence of this
-            technique lies in dynamically altering the perspective origin, thus
-            creating a captivating visual experience. By carefully adjusting
-            this property, the project achieves a seamless blend of foreground
-            and background elements, offering users a striking sense of depth
-            and immersion.
-          </p>
-        </Dialog>
-      )}
+      <Dialog
+        heading={'About'}
+        open={isActive}
+        onClickClose={() => {
+          setIsActive(false);
+        }}
+      >
+        <p>
+          This project showcases a unique approach to implementing the parallax
+          effect through the manipulation of the "perspective-origin" CSS
+          property within the encompassing container. The essence of this
+          technique lies in dynamically altering the perspective origin, thus
+          creating a captivating visual experience. By carefully adjusting this
+          property, the project achieves a seamless blend of foreground and
+          background elements, offering users a striking sense of depth and
+          immersion.
+        </p>
+      </Dialog>
     </>
   );
 };
